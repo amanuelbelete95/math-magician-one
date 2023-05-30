007045f8e156ab5911a369b324e91a8d0f8b7807
yared tekle <yaredtekle022@gmail.com>
2023-05-30 22:48:28 +0300

    Create a static  calculator UI component

---
+++ src/components/calculator.js
@@ -0,0 +1,37 @@
+function Calculator() {
+  return (
+    <div className="container">
+      <div className="wraper">
+
+        <div className="screen">0</div>
+
+        <div className="btn light-gray">AC</div>
+        <div className="btn light-gray">+/-</div>
+        <div className="btn light-gray">%</div>
+        <div className="btn orange">/</div>
+
+        <div className="btn">7</div>
+        <div className="btn">8</div>
+        <div className="btn">9</div>
+        <div className="btn orange">x</div>
+
+        <div className="btn">4</div>
+        <div className="btn">5</div>
+        <div className="btn">6</div>
+        <div className="btn orange">-</div>
+
+        <div className="btn">1</div>
+        <div className="btn">2</div>
+        <div className="btn">3</div>
+        <div className="btn orange">+</div>
+
+        <div className="btn">0</div>
+        <div className="btn" />
+        <div className="btn">.</div>
+        <div className="btn orange">=</div>
+
+      </div>
+    </div>
+  );
+}
+export default Calculator;
