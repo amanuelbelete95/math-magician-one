97a5671331cf987eae69881f6b36e8ccb2b75992
yared tekle <yaredtekle022@gmail.com>
2023-05-31 22:42:09 +0300

    Add events to the buttons of calculator

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,37 +1,61 @@
-function Calculator() {
+import React, { useState } from 'react';
+import Button from './myButton';
+import calculate from '../logic/calculate';
+
+const Calculator = () => {
+  const [display, setDisplay] = useState('0');
+  const [calculatorData, setCalculatorData] = useState({});
+  const handleButtonClick = (buttonName) => {
+    const newData = calculate(calculatorData, buttonName);
+    if (buttonName === '=') {
+      if (newData.total) {
+        setDisplay(newData.total);
+      } else {
+        setDisplay('0');
+      }
+      setCalculatorData({});
+    } else if (['+', '-', 'x', '÷'].includes(buttonName)) {
+      setDisplay(buttonName);
+      setCalculatorData({
+        total: display,
+        next: null,
+        operation: buttonName,
+      });
+    } else {
+      if (newData.next) {
+        setDisplay(newData.next);
+      } else {
+        setDisplay('0');
+      }
+      setCalculatorData(newData);
+    }
+  };
   return (
     <div className="container">
-      <div className="wraper">
-
-        <div className="screen">0</div>
-
-        <div className="btn light-gray">AC</div>
-        <div className="btn light-gray">+/-</div>
-        <div className="btn light-gray">%</div>
-        <div className="btn orange">/</div>
-
-        <div className="btn">7</div>
-        <div className="btn">8</div>
-        <div className="btn">9</div>
-        <div className="btn orange">x</div>
-
-        <div className="btn">4</div>
-        <div className="btn">5</div>
-        <div className="btn">6</div>
-        <div className="btn orange">-</div>
-
-        <div className="btn">1</div>
-        <div className="btn">2</div>
-        <div className="btn">3</div>
-        <div className="btn orange">+</div>
-
-        <div className="btn">0</div>
-        <div className="btn" />
-        <div className="btn">.</div>
-        <div className="btn orange">=</div>
-
+      <div className="wrapper">
+        <div className="screen">{display}</div>
+        <Button className="btn light-gray" onClick={() => handleButtonClick('AC')}>AC</Button>
+        <Button className="btn light-gray" onClick={() => handleButtonClick('+/-')}>+/-</Button>
+        <Button className="btn light-gray" onClick={() => handleButtonClick('%')}>%</Button>
+        <Button className="btn orange" onClick={() => handleButtonClick('/')}>/</Button>
+        <Button className="btn" onClick={() => handleButtonClick('7')}>7</Button>
+        <Button className="btn" onClick={() => handleButtonClick('8')}>8</Button>
+        <Button className="btn" onClick={() => handleButtonClick('9')}>9</Button>
+        <Button className="btn orange" onClick={() => handleButtonClick('x')}>x</Button>
+        <Button className="btn" onClick={() => handleButtonClick('4')}>4</Button>
+        <Button className="btn" onClick={() => handleButtonClick('5')}>5</Button>
+        <Button className="btn" onClick={() => handleButtonClick('6')}>6</Button>
+        <Button className="btn orange" onClick={() => handleButtonClick('-')}>-</Button>
+        <Button className="btn" onClick={() => handleButtonClick('1')}>1</Button>
+        <Button className="btn" onClick={() => handleButtonClick('2')}>2</Button>
+        <Button className="btn" onClick={() => handleButtonClick('3')}>3</Button>
+        <Button className="btn orange" onClick={() => handleButtonClick('+')}>+</Button>
+        <Button className="btn zero" onClick={() => handleButtonClick('0')}>0</Button>
+        <Button className="btn" onClick={() => handleButtonClick('.')}>.</Button>
+        <Button className="btn orange" onClick={() => handleButtonClick('=')}>=</Button>
       </div>
     </div>
   );
-}
+};
+
 export default Calculator;
